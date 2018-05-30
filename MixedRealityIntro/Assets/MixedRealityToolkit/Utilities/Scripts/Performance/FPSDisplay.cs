d67f36e15f58d5340469273b5f495e61ad1edc40
Chris Darcy <cdarcy@paloaltonetworks.com>
2018-05-30 10:22:04 -0700

    added first project

---
+++ MixedRealityIntro/Assets/MixedRealityToolkit/Utilities/Scripts/Performance/FPSDisplay.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public float DisplayInterval = 0.5f;
+    public string FPSToggleKey = "m";
+
+    UnityEngine.UI.Text fpsGUIText;
+    private bool show = true;
+    float accumDelta = 0.0f;
+    int frames = 0;
+    private float lastDisplayUpdate = 0;
+
+    void Awake()
+    {
+        fpsGUIText = GetComponent<UnityEngine.UI.Text>();
+
+        // If on Hololense, we recommend the following setting
+        //UnityEngine.VR.WSA.HolographicSettings.ActivateLatentFramePresentation(true);
+    }
+
+    void Update()
+    {
+        if (Input.inputString.Contains(FPSToggleKey))
+        {
+            show = !show;
+        }
+
+        accumDelta += Time.deltaTime;
+        frames++;
+
+        if (Time.time - lastDisplayUpdate < DisplayInterval) // only update once every display interval
+        {
+            return;
+        }
+
+        float msec = accumDelta * 1000.0f;
+        float fps = (float)frames / accumDelta;
+
+        if (fpsGUIText != null)
+        {
+            if (show)
+            {
+                fpsGUIText.text = string.Format("{0:0.0} ms ({1:0.}/{2:0.} fps) v:{3:0.00} q:{4}", msec / frames, fps, XRDevice.refreshRate, ViewportScaling.CurrentViewport, QualityManager.QualityLevelName);
+            }
+            else
+            {
+                fpsGUIText.text = "";
+            }
+        }
+        
+        //Reset
+        lastDisplayUpdate = Time.time;
+        frames = 0;
+        accumDelta = 0;
+    }
+}
---
+++ MixedRealityIntro/Assets/MixedRealityToolkit/Utilities/Scripts/Performance/ViewportScaling.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+
+public class ViewportScaling : MonoBehaviour
+{
+    public static float CurrentViewport = 1.0f;
+    private volatile float targetViewport = 1.0f;
+    [SerializeField] private float minViewportScaleFactor = 0.05f;  
+
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    void Start()
+    {
+        //Usefull for HoloLens, make sure to turn on mic capabilities in order to use it
+        //SetupKeywords();
+    }
+
+    public void SetupKeywords()
+    {
+        // Viewport kewords
+        for (int i = 1; i < 10; ++i)
+        {
+            float v = (float)i / 10.0f;
+            keywords.Add("Set viewport to point " + i, () =>
+            {
+                targetViewport = v;
+            });
+
+            keywords.Add("Set viewport to point " + i + " five", () =>
+            {
+                targetViewport = v + 0.05f;
+            });
+        }
+
+        keywords.Add("Set viewport to one", () =>
+        {
+            targetViewport = 1.0f;
+        });
+
+        keywords.Add("Set viewport to point zero five", () =>
+        {
+            targetViewport = 1.0f;
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    void Update ()
+    {
+        if (Input.inputString == "[")
+        {
+            this.targetViewport -= 0.05f;
+            this.targetViewport = Mathf.Clamp(this.targetViewport, this.minViewportScaleFactor, 1.0f);
+        }
+        else if (Input.inputString == "]")
+        {
+            this.targetViewport += 0.05f;
+            this.targetViewport = Mathf.Clamp(this.targetViewport, this.minViewportScaleFactor, 1.0f);
+        }
+
+        if (this.targetViewport != CurrentViewport)
+        {
+            SetViewport(this.targetViewport);
+        }
+	}
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    void SetViewport(float val)
+    {
+        this.targetViewport = val;
+        this.targetViewport = Mathf.Clamp(this.targetViewport, this.minViewportScaleFactor, 1.0f);
+
+        UnityEngine.XR.XRSettings.renderViewportScale = this.targetViewport;
+        CurrentViewport = this.targetViewport;
+    }
+}
