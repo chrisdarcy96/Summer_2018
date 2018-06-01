246beba31556c497c092a385046bb51c8d2458f1
chrisdarcy96 <chrisdarcy96@gmail.com>
2018-06-01 11:14:06 -0700

    Spatial Mapping and Understanding complete

---
+++ HoloLens Dev 101/Assets/Scripts/ObjectPlacer.cs
@@ -0,0 +1,45 @@
+﻿//---------------------
+// Source code from https://www.cameronvetter.com/2017/01/30/hololens-tutorial-object-placement-and-scaling/
+// Chris Darcy, June 2018
+//
+// --------------------
+
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using System;
+
+public class ObjectPlacer : MonoBehaviour {
+
+    public SpatialUnderstandingCustomMesh SpatialUnderstandingMesh;
+    private bool _timeToHideMesh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_timeToHideMesh)
+        {
+            SpatialUnderstandingState.Instance.HideText = true;
+            HideGridEnableOcclusion();
+            _timeToHideMesh = false;
+        }
+	}
+
+    private void HideGridEnableOcclusion()
+    {
+        SpatialUnderstandingMesh.DrawProcessedMesh = false;
+    }
+
+    public void CreateScene()
+    {
+        if (!SpatialUnderstanding.Instance.AllowSpatialUnderstanding)
+        {
+            return;
+        }
+        _timeToHideMesh = true;
+    }
+}
--- notes.md
+++ notes.md
@@ -3,3 +3,11 @@
 Mixed Reality toolkit recommends Unity 2017.2.1p2
 
 Watch later on network [visualization:] (https://www.youtube.com/watch?v=FLiv3xnEepw)
+
+
+C# code: what in the fuck does this do? "recognizer.Tapped += (args) =>"
+
+What is difference between between position and localposition on gameobject?
+
+WTF is Quatenion?
+
