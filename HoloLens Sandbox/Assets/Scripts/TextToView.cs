e2feb087dcc7886cf220021524cb18b0c68bf343
chrisdarcy96 <chrisdarcy96@gmail.com>
2018-06-18 16:32:40 -0700

    zach saved my life

---
+++ HoloLens Sandbox/Assets/Scripts/TextToView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TextToView : MonoBehaviour {
+
+    public string path = "Assets/info.txt";
+
+	// Use this for initialization
+	void Start () {
+        ReadFile();
+	}
+	
+
+
+    
+    void ReadFile()
+    {
+        using (StreamReader sr = new StreamReader(File.OpenRead(path)))
+        {
+            string line = sr.ReadLine();
+            while(line != null)
+            {
+                print(line);
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
