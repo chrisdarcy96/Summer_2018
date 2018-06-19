fe67d31dee676ed76772a9eeb66d0a678c7a8a78
Chuck <jctlynch@gmail.com>
2018-06-19 16:15:11 -0700

    Changed influencers list type to be GameObject to ease interaction w other components.

--- Nodes/Assets/Scripts/SphereRepulsion.cs
+++ Nodes/Assets/Scripts/SphereRepulsion.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 /// <summary>
 /// Script that makes spheres repel from a central sphere. Based off of Coulomb's Law - with simplifications.
@@ -19,10 +21,12 @@ public class SphereRepulsion : MonoBehaviour
     public float fixedY = 1;
     public float currentDistance; // PUBLIC FOR DEBUGGING
 
+
     // Private variables
     private bool isTargetReached = true;
-    private bool isInRepulsionZone = false; // True when we are in the repulsion zone
-    private Vector3 targetedPosition; // Target position from the repulsion effect
+    private bool influenced = false; // True when we are in the repulsion zone
+    private List<GameObject> influencers = new List<GameObject>();
+
 
     // Use this for initialization
     void Start()
@@ -37,9 +41,28 @@ public class SphereRepulsion : MonoBehaviour
 
         // If we have attained the distance we desire, cut the force.
         this.currentDistance = Vector3.Distance(this.transform.position, this.SphereCentral.transform.position);
+        if(this.currentDistance >= maxRange)
+        {
+            forceHalt(this);
+        }
+        //Otherwise, we want to calculate the estimated force based off of all units within our collider.
+        calcForce(influencers);
 
 
+    }
 
+    private void calcForce(List<GameObject> influencers)
+    {
+        // Create a net charge force in 3d based off of the influence of multiple other gameObjects
+        foreach(GameObject influencer in influencers)
+        {
+
+        }
+    }
+
+    private void forceHalt(SphereRepulsion sphereRepulsion)
+    {
+        throw new NotImplementedException();
     }
 
     void OnTriggerEnter(Collider other)
@@ -50,7 +73,7 @@ public class SphereRepulsion : MonoBehaviour
 
         
         this.SphereCentral = other.gameObject; // Linking the central sphere gameObject
-        this.isInRepulsionZone = true;
+        this.influenced = true;
         this.isTargetReached = false;
 
         // Compute new position
@@ -59,10 +82,11 @@ public class SphereRepulsion : MonoBehaviour
         Vector3 direction = positionSphereCentral - position;
         Vector3 normalized = Vector3.Normalize(direction) * -1; // -1 because we want to repulse to the opposite side
 
-        print(sphereCentralDistance);
-        this.targetedPosition = normalized * sphereCentralDistance;
-        this.targetedPosition += normalized * 0.01F; // Adding 0.01F because we want the sphere to go out of the zone
-
+        // Throw the new collider into the list of objects that this cares about
+        if (!influencers.Contains(other.gameObject))
+        {
+            influencers.Add(other.gameObject);
+        }
 
     }
 
@@ -79,6 +103,13 @@ public class SphereRepulsion : MonoBehaviour
     {
         // Snap the position to the exact value
         Debug.LogWarning("EXITING TRIGGER ZONE");
-        this.isInRepulsionZone = false;
+        this.influenced = false;
+
+        // Remove the influencer if it's in the influencers list
+        if (influencers.Contains(other.gameObject))
+        {
+            influencers.Add(other.gameObject);
+        }
+
     }
 }
