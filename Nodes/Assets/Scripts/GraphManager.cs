8b78bbd927fe300ff7571f006f591555905132a6
Chuck <jctlynch@gmail.com>
2018-06-19 10:46:58 -0700

    Created Scripts folder, added repulsion colliders
    Also imported root of DrawCylinder into GraphManager

--- Nodes/Assets/Scripts/GraphManager.cs
+++ Nodes/Assets/Scripts/GraphManager.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 /*
  * GraphManager Class
@@ -16,13 +19,17 @@ public class GraphManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        // Associate our DrawCylinders nodes with this script
-        GameObject nexus = GameObject.Find("Nexus");
-        List<GameObject> nodes = nexus.GetComponent<DrawCylinders>().nodes;
+        // Associate our DrawCylinders nodes and root with this script
+        GameObject nexus = GameObject.Find("LineNexus");
+        DrawCylinders dc = nexus.GetComponent<DrawCylinders>();
+        List<GameObject> nodes = dc.nodes;
+        GameObject root = dc.root;
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+  
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
