f90bba851dad35b7b1284f4d4a1615bfc613b20b
Chuck <jctlynch@gmail.com>
2018-06-29 13:53:48 -0700

    Removed all linkages to GameCtrlHelper, and commented out the script itself.

--- Nodes/Assets/Scripts/GraphController.cs
+++ Nodes/Assets/Scripts/GraphController.cs
@@ -20,7 +20,6 @@ public class GraphController : MonoBehaviour {
 
     private static GameController gameControl;
     //private static GameCtrlUI gameCtrlUI;
-    private static GameCtrlHelper gameCtrlHelper;
 
     [SerializeField]
     private bool allStatic = false;
@@ -117,21 +116,7 @@ public class GraphController : MonoBehaviour {
         }
     }
 
-    public bool DebugRepulse
-    {
-        get
-        {
-            return debugRepulse;
-        }
-        set
-        {
-            if (debugRepulse != value)
-            {
-                debugRepulse = value;
-                DebugAllNodes();
-            }
-        }
-    }
+
 
     public float GlobalGravityBullet
     {
@@ -218,29 +203,6 @@ public class GraphController : MonoBehaviour {
     }
 
 
-
-    void DebugAllNodes()
-    {
-        if (DebugRepulse)
-        {
-            foreach (GameObject debugObj in debugObjects)
-            {
-                debugObj.SetActive(true);
-                if (debugObj.name == "debugRepulseObj")
-                {
-                    float sphereDiam = gameCtrlHelper.GetRepulseSphereDiam();
-                    debugObj.transform.localScale = new Vector3(sphereDiam, sphereDiam, sphereDiam);
-                }
-            }
-        }
-        else
-        {
-            foreach (GameObject debugObj in debugObjects)
-            {
-                debugObj.SetActive(false);
-            }
-        }
-    }
 
     public void ResetWorld()
     {
