25d674dea52eb1095c0a2eab3fcfbc1e11260710
Chuck <jctlynch@gmail.com>
2018-07-02 14:29:00 -0700

    Imported most useful prefabs and shredded most UI-focused FD scripts
    This includes the SelctionManager, GameCtrl* scripts, and any scripts I kept from older versions of Nodes (pre-GitHub clone of the Force Direction Assets)

---
+++ CSRPFinal/Assets/Scripts/Node.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public abstract class Node : MonoBehaviour
+{
+    [SerializeField]
+    protected static bool verbose = true;
+
+    protected static GraphController graphControl;
+
+    private string id;
+    private string text;
+    private string type;
+
+    public string Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Text
+    {
+        get
+        {
+            return text;
+        }
+        set
+        {
+            text = value;
+        }
+    }
+
+    public string Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            type = value;
+        }
+    }
+
+    protected abstract void doGravity();
+
+    protected abstract void doRepulse();
+
+    protected virtual void Start()
+    {
+        graphControl = FindObjectOfType<GraphController>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!graphControl.AllStatic && graphControl.RepulseActive)
+            doRepulse();
+
+        if (!graphControl.AllStatic)
+            doGravity();
+    }
+}
